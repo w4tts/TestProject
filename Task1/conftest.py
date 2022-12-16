bc8df74f5600cd371e96e9376a96b219d0c8864d
w4tts <xradonx@gmail.com>
2022-12-16 16:10:12 +0300

    refactoring

--- Task1/conftest.py
+++ Task1/conftest.py
@@ -1,7 +1,14 @@
 import pytest
+import random
 
 
-@pytest.fixture()
-def count():
-    return 42
+@pytest.fixture(scope="function")
+def random_number():
+    number = random.randint(1, 5)
+    return number
 
+
+@pytest.fixture(scope="session")
+def random_number_2():
+    number_s = random.randint(1, 500)
+    return number_s
---
+++ Task1/test_example.py
@@ -0,0 +1,22 @@
+def test_1(random_number):
+    assert 1 == random_number
+
+
+def test_2(random_number):
+    assert 2 == random_number
+
+
+def test_3(random_number):
+    assert 3 == random_number
+
+
+def test_4(random_number_2):
+    assert 1 == random_number_2
+
+
+def test_5(random_number_2):
+    assert 2 == random_number_2
+
+
+def test_6(random_number_2):
+    assert 3 == random_number_2
